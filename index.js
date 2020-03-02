f69f3197ccfe42aee58ab2da592519f056d81a16
Lanczi Dominik <nhcmon@gmail.com>
2020-03-02 13:19:51 +0100

    Added comments

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+//
+// Adding hexo-fs
+//
 var fs = require('hexo-fs');
 
+//
+// Taking the objects from the config file
+//
 let config = Object.assign({destination: 'main', path: 'themes/landscape/source/css/', files: 'styles', minify: true}, hexo.config.css_merge);
 let fileNames = Object.assign({}, config.files);
 
+//
+// Declaring variables
+//
 var allcontent = '';
 var contents = [];
 
+//
+// Getting the data from the CSS files
+//
 for (var i in fileNames)
 {
     contents[i] = config.path + fileNames[i] + '.css';
@@ -15,6 +27,9 @@ for (i = 0; i < contents.length; i++) {
     allcontent = allcontent + fs.readFileSync(contents[i]) + ' ';
 }
 
+//
+// CSS minification by the help of CleanCSS
+//
 if (config.minify) {
     var CleanCSS = require('clean-css');
     var input = allcontent;
@@ -22,7 +37,9 @@ if (config.minify) {
     var output = new CleanCSS(options).minify(input);
 }
 
-
+//
+// Returning the final CSS file when generating in Hexo
+//
 hexo.extend.generator.register('css-merge', function (locals) {
 
     return {
