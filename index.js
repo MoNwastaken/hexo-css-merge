0be83652b2609ef2cf38d642e73be397f50c8d0c
Lanczi Dominik <nhcmon@gmail.com>
2020-03-02 11:37:37 +0100

    Added compatibility with config.yml

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
 var fs = require('hexo-fs');
+
+let config = Object.assign({destination: 'main', path: 'themes/landscape/source/css/', files: 'styles', minify: true}, hexo.config.css_merge);
+let fileNames = Object.assign({}, config.files);
+
 var allcontent = '';
 var contents = [];
 
-for (i = 0; i < config.css_merge.length; i++) {
-    contents.push(config.css_merge[i]);
+for (var i in fileNames)
+{
+    contents[i] = config.path + fileNames[i] + '.css';
 }
 
 for (i = 0; i < contents.length; i++) {
     allcontent = allcontent + fs.readFileSync(contents[i]) + ' ';
 }
 
+if (config.minify) {
+    var CleanCSS = require('clean-css');
+    var input = allcontent;
+    var options = {  };
+    var output = new CleanCSS(options).minify(input);
+}
 
-var CleanCSS = require('clean-css');
-var input = allcontent;
-var options = {  };
-var output = new CleanCSS(options).minify(input);
 
 hexo.extend.generator.register('css-merge', function (locals) {
 
     return {
 
-        path: '/css/main.css',
+        path: '/css/' + config.destination + '.css',
         data: function(){
-            return output.styles;
+            if (config.minify) {
+                return output.styles;
+            }
+            else {
+                return allcontent;
+            }
         }
 
     };
